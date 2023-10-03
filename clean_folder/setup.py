b22c7b6e84143cfef44c53d2b2358883149c76d8
lotuskolobok <lotus.kolobok@gmail.com>
2023-10-03 12:14:43 +0300

    update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='clean',
-      version='1.0.0',
+setup(name='clean_folder',
+      version='1.0.1',
       description='Clean folder',
       url='https://github.com/lotuskolobok/HomeWork6/tree/main/clean_folder',
       author='Kolobok',
       author_email='kolobok@example.com',
       license='MIT',
-      packages=['clean_folder']
+      packages=['clean_folder'],
+      entry_points={'console_scripts': ['cleanfolder = clean_folder.clean:main']}
       )
