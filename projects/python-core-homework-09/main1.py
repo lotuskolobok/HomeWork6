9c31c7599e30921fe4d359df2f0db82ec8bc44e8
lotuskolobok <lotus.kolobok@gmail.com>
2023-10-20 18:16:00 +0300

    update HW_09

--- projects/python-core-homework-09/main1.py
+++ projects/python-core-homework-09/main1.py
@@ -1,6 +1,21 @@
-
 phone_book = {}
 exit_cmd = ['good bye', 'close', 'exit', 'quit', 'end']
+
+def input_error(Exception):
+    try:
+        pass
+
+    except ValueError:
+        print ('-=ERROR-=: Incorrect value')
+
+    except KeyError:
+        print ('-=ERROR-=: Enter user name')
+
+    except IndexError:
+        print ('-=ERROR-=: Incorrect count of args')
+
+    except Warning:
+        print ('-=ERROR-=: Incorrect command')
 
 def add_phone(my_string: str):
     tmp = my_string.split()
@@ -17,7 +32,7 @@ def add_phone(my_string: str):
     else:
         raise IndexError
 
-def change_phone(my_string: str):
+def change_phone(my_string:str):
     tmp = my_string.split()
     if len(tmp) == 2:
         name = tmp[0]
@@ -50,62 +65,36 @@ def phone_show(my_string: str):
         raise IndexError
 
 
+
+@input_error
 def call_bot():
-
-    is_break = False
-
-    while True:
-        
-        try:
-            
-            command = input('Enter your command: ').lower()
-
-            if command == 'hello':
-                result = 'How can I help you?'
-            
-            elif command in exit_cmd:
-                result = '-'*10 + '\nBye-Bye!:)\n' + '-'*10
-                is_break = True
-                
-            elif command.startswith('add'):
-                result = add_phone(command.removeprefix('add'))
-
-            elif command.startswith('change'):
-                result = change_phone(command.removeprefix('change'))
-            
-            elif command.startswith('show all'):
-                result = phone_book
-
-            elif command.startswith('phone'):
-                result = phone_show(command.removeprefix('phone'))
-
-            else:
-                raise Warning
-
-            
-            print (result)
-            
-            if is_break:
-                break
-
-        except ValueError:
-            print ('-=ERROR-=: Incorrect value')
-        
-        except KeyError:
-            print ('-=ERROR-=: Enter user name')
-        
-        except IndexError:
-            print ('-=ERROR-=: Incorrect count of args')
-        
-        except Warning:
-            print ('-=ERROR-=: Incorrect command')
-        
-        
     
+    while True:
+        is_break = False
+        command = input('Enter your command: ').lower()
+
+        if command == 'hello':
+            result = 'How can I help you?'
+        elif command in exit_cmd:
+            result = '-'*10 + '\nBye-Bye!:)\n' + '-'*10
+            is_break = True
+        elif command.startswith('add'):
+            result = add_phone(command.removeprefix('add'))
+        elif command.startswith('change'):
+            result = change_phone(command.removeprefix('change'))
+        elif command.startswith('show all'):
+            result = phone_book
+        elif command.startswith('phone'):
+            result = phone_show(command.removeprefix('phone'))
+        else:
+            raise Warning
+
+        print (result)
+
+        if is_break:
+            break
+
+
 if __name__ == '__main__':
 
     call_bot()
-
-    
-
-
