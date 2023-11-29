551ab975df9f7f9a422a0e3f83cc90d9bc2a5e18
lotuskolobok <lotus.kolobok@gmail.com>
2023-11-29 12:43:43 +0200

    Home Work 10 Update 3

--- projects/python-core-homework-10/main.py
+++ projects/python-core-homework-10/main.py
@@ -9,12 +9,7 @@ class Field:
 
 
 class Name(Field):
-    def __init__(self, value):
-        self.value = value
-    
-    def __str__(self):
-        return str(self.value)
-
+    pass
 
 class Phone(Field):
     def __init__(self, value):
